5c2ceb7e9d7647079cff101e7c49ebaf04b4c36d
lj90go <805007406@qq.com>
2015-07-28 09:43:59 +0800

    Initial Commit

---
+++ GoUniversity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoUniversity
+//
+//  Created by edutech on 15/7/28.
+//  Copyright (c) 2015年 luojie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
