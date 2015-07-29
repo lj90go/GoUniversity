3ddb954f3630e3ba4cd22af067db6db0657e67f9
lj90go <luojia1990go@gmail.com>
2015-07-29 08:35:12 +0800

    Update AppDelegate.m

--- GoUniversity/AppDelegate.m
+++ GoUniversity/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog("can you get this modify");
     return YES;
 }
 
